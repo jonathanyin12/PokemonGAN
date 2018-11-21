d0988283c3233c09814feae36120fce97d762eee
Jonathan Yin <jonathanyin.12@gmail.com>
2018-11-20 20:58:27 -0500

    updated preprocessing

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,8 +1,8 @@
 import os
 from PIL import Image
 
-pokemon_dataset_dir = 'pokemon_dataset'
-processed_dataset = 'HQ_dataset'
+pokemon_dataset_dir = 'raw_dataset'
+processed_dataset = 'processed_dataset'
 white_background = processed_dataset + '/white_background'
 flip = processed_dataset + '/flip'
 rotate = processed_dataset + '/rotate'
@@ -21,19 +21,21 @@ if not os.path.exists(rotate_dir):
                 os.makedirs(rotate_dir)
 
 
+# Converts images from RGBA to RGB
 def rgba2rgb(image):
     convert_image = image.convert('RGBA') # Convert to RGBA to make sure it's a supported alpha channel
     new_img = Image.new("RGB", convert_image.size, (255, 255, 255))
     new_img.paste(convert_image, mask=convert_image.split()[3]) # 3 is the alpha channel
-
     return new_img
 
 
+# Flips the images
 def flip_image(image):
     new_img = image.transpose(Image.FLIP_LEFT_RIGHT)
     return new_img
 
 
+# Rotates the images
 def rotate_image(image, degree):
     new_img = image.convert('RGBA') # convert to RGBA to get transparent background
     new_img = new_img.rotate(degree)
@@ -42,18 +44,21 @@ def rotate_image(image, degree):
     return background
 
 
+# Creates RGB images
 for each in os.listdir(pokemon_dir):
     img = Image.open(pokemon_dir + '/' + each, 'r')
     img.load()
-    rgb_img = rgba2rgb(img).resize((1024, 1024),Image.LANCZOS)
+    rgb_img = rgba2rgb(img)
     rgb_img.save(white_background + '/' + each[:-3] + 'jpg', 'JPEG', quality=100)
 
-for each in os.listdir(white_background_dir):
-    img = Image.open(white_background_dir + '/' + each, 'r')
+# Creates flipped RGB images
+for each in os.listdir(white_background):
+    img = Image.open(white_background + '/' + each, 'r')
     img.load()
-    flip_img = flip_image(img).resize((1024, 1024),Image.LANCZOS)
+    flip_img = flip_image(img)
     flip_img.save(flip + '/' + each[:-4] + '-f.jpg', 'JPEG', quality=100)
-'''
+
+# Creates rotated images
 for each in os.listdir(white_background_dir):
     img = Image.open(white_background_dir + '/' + each, 'r')
     img.load()
@@ -79,6 +84,7 @@ for each in os.listdir(white_background_dir):
     rotate10degcw = rotate_image(img, -10)
     rotate10degcw.save(rotate + '/' + each[:-4] + '_cwr10.jpg', 'JPEG', quality=100)
 
+# Creates rotated + flipped images
 for each in os.listdir(flip_dir):
     img = Image.open(flip + '/' + each, 'r')
     img.load()
@@ -104,4 +110,4 @@ for each in os.listdir(flip_dir):
     rotate10degcw = rotate_image(img, -10)
     rotate10degcw.save(rotate + '/' + each[:-4] + '_cwr10.jpg', 'JPEG', quality=100)
 
-'''
+
